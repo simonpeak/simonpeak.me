7e471bdb3c6e974ae06f9f8c1c0516dbfc86c91d
Simon Peak <simonpeak@gmail.com>
2025-01-09 11:11:42 +0000

    Add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
+  markdown: { remarkPlugins: [remarkReadingTime] }
 });
